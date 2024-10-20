5936fb74cef0176ca030221a5662878fc8078374
Jeffrey MacPherson <jeff_macpherson95@hotmail.com>
2024-10-20 01:58:58 -0300

    added the 'walking' animation

--- core/src/main/java/com/engine/project1/Player.java
+++ core/src/main/java/com/engine/project1/Player.java
@@ -16,6 +16,8 @@ public class Player extends InputAdapter {
     public float xPos;
     public float yPos;
     public float speed;
+    public boolean isMoving;
+    public int lastDirection;
 
     public Player(TextureAtlas atlas) {
         heroWidth = 64;
@@ -23,6 +25,8 @@ public class Player extends InputAdapter {
         xPos = 0;
         yPos = 0;
         speed = 100.0f;
+        isMoving = false;
+        lastDirection = Input.Keys.DOWN;
         playerSprite = new Sprite(atlas.findRegion("hero"));
         playerSprite.setSize(heroHeight, heroWidth);
     }
@@ -32,32 +36,64 @@ public class Player extends InputAdapter {
     }
 
     public void inputHandling(float deltaTime, TextureAtlas atlas) {
+        isMoving = false;
+
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             yPos += speed * deltaTime;
             playerSprite = new Sprite(atlas.findRegion("hero_backwards"));
             if (yPos > Main.HEIGHT - heroHeight) {
                 yPos = Main.HEIGHT - heroHeight;
             }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            yPos -= speed * deltaTime;
-            playerSprite = new Sprite(atlas.findRegion("hero"));
-            if (yPos < 0) {
-                yPos = 0;
+
+            isMoving = true;
+            lastDirection = Input.Keys.UP;
+
+            } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                yPos -= speed * deltaTime;
+                playerSprite = new Sprite(atlas.findRegion("hero"));
+                if (yPos < 0) {
+                    yPos = 0;
+                }
+
+                isMoving = true;
+                lastDirection = Input.Keys.DOWN;
             }
-        }
+
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             xPos -= speed * deltaTime;
-            playerSprite = new Sprite(atlas.findRegion("hero_facing_left"));
+            playerSprite = new Sprite(atlas.findRegion("hero_walking_left"));
             if (xPos < 0) {
                 xPos = 0;
             }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+
+            isMoving = true;
+            lastDirection = Input.Keys.LEFT;
+
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             xPos += speed * deltaTime;
-            playerSprite = new Sprite(atlas.findRegion("hero_facing_right"));
+            playerSprite = new Sprite(atlas.findRegion("hero_walking_right"));
             if (xPos > Main.WIDTH - heroWidth) {
                 xPos = Main.WIDTH - heroWidth;
+            }
+
+            isMoving = true;
+            lastDirection = Input.Keys.RIGHT;
+        }
+
+        if (!isMoving) {
+            switch (lastDirection) {
+                case Input.Keys.UP:
+                    playerSprite = new Sprite(atlas.findRegion("hero_backwards"));
+                    break;
+                case Input.Keys.DOWN:
+                    playerSprite = new Sprite(atlas.findRegion("hero"));
+                    break;
+                case Input.Keys.LEFT:
+                    playerSprite = new Sprite(atlas.findRegion("hero_facing_left"));
+                    break;
+                case Input.Keys.RIGHT:
+                    playerSprite = new Sprite(atlas.findRegion("hero_facing_right"));
+                    break;
             }
         }
 
